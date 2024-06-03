5de3b36f60685b2cabef7221e3b6652d6925b1e2
Subhajit Kundu <subha60kundu@gmail.com>
2024-06-03 17:13:15 +0530

    core: Adding favicon and metadata

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,17 @@ import Testimonials from '@/components/Testimonials'
 import FaqSection from '@/components/FaqSection'
 import CTASection from '@/components/CTASection'
 import Footer from '@/components/Footer'
+import {Metadata} from 'next'
+
+export const metadata: Metadata = {
+	title: 'Batch System Assignment | UIFry',
+	description: 'This is the assignment for the frontend development internship at Batch System. This project is crafted with Next.js, Tailwind CSS, Framer Motion, and TypeScript. This is a mobile responsive replica of the Figma design that was provided with the assignment details',
+	openGraph: {
+		title: 'Batch System Assignment | UIFry',
+		description: 'This is the assignment for the frontend development internship at Batch System. This project is crafted with Next.js, Tailwind CSS, Framer Motion, and TypeScript. This is a mobile responsive replica of the Figma design that was provided with the assignment details',
+		type: 'website'
+	}
+}
 
 const HomePage = () => {
 	return (
